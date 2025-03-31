2ab4cac74041840375adabce9318eca97c7cb943
Ayman ALKHATIB <aemn22384@gmail.com>
2025-04-01 00:13:26 +0200

    add Navigation component with responsive styles and logo assets

---
+++ app/_components/Navigation.tsx
@@ -0,0 +1,48 @@
+"use client"
+import  { useState } from 'react'
+import styles from './Navigation.module.css'
+import Link from "next/link";
+import Image from "next/image";
+import MenuBurger from './MenuBurger';
+
+function Navigation() {
+    const [isActive, setIsActive] = useState(false);
+
+    function handleMenuToggle(){
+        setIsActive(!isActive);
+    };
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.logo}>
+                <Image
+                    src="/image.png"
+                    alt="Logo"
+                    width={50}
+                    height={50}
+                />
+            </div>
+            <span>Syrian Emergency Medicine Initiative</span>
+         
+            <ul className={`${isActive ? styles.active :""}`}>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/events">Events</Link>
+              </li>
+              <li>
+                <Link href="/Resources">Resources</Link>
+              </li>
+              <li>
+                <Link href="/support">Support Us</Link>
+              </li>
+            </ul>
+            <MenuBurger isActive={isActive} onMenuToggle={handleMenuToggle}/>
+          </nav>
+    )
+}
+
+export default Navigation
