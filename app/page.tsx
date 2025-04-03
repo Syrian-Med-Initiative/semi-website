9eb537b7ce6b3658c22355cf6916078f7bd49a25
Ayman ALKHATIB <aemn22384@gmail.com>
2025-04-03 23:40:30 +0200

    write a temp text in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-   <div>home page</div>
+    <section style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
+        <h1 className="text-4xl font-bold text-white text-center pt-16">
+          Welcome to Our App
+        </h1>
+        <p className="text-lg text-white text-center mt-4">
+          Your journey starts here. Explore our features and enjoy the experience.
+        </p>
+    </section>
+
   );
 }
