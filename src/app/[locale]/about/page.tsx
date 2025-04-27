5d7f749a05bcee9e75719d8d43bdc041df91da18
Ayman Alkhatib <91638309+ayman-alkhatib@users.noreply.github.com>
2025-04-28 01:57:41 +0200

    Dev (#8)
    * Root layout (#1)
    
    * remove PostCSS configuration and Tailwind CSS dependencies; update global styles and container layout
    
    * add MenuBurger component with responsive styles
    
    * add Navigation component with responsive styles and logo assets
    
    * add Header component with Navigation integration
    
    * add Header component to RootLayout
    
    * update primary color in global styles to improve UI aesthetics
    
    * add Footer and SocialsIcons components with responsive styles
    
    * add Footer and main container to RootLayout for improved structure
    
    * add react-icons dependency for enhanced icon support
    
    * refactor/add all the component of rootLayout to the folder rootLayout
    
    * refactor: simplify SocialsIcons styles by removing unnecessary padding and borders
    
    * style: enhance SocialsIcons layout with improved flex alignment for links
    
    * write a temp text in the main page
    
    * remove the container from the main element
    
    * update logo dimensions and replace image source; add new logo files
    
    * Init/components (#3)
    
    * add EventCard component with styles and event data type
    
    * refactor EventCard component to use EventData type for improved type safety
    
    * update color variables in globals.css for improved theming
    
    * add MemberCard component with styles and social media links
    
    * add global styles for carousel component and special heading
    
    * change footer position from fixed to sticky
    
    * add Card component with styles and functionality
    
    * add Button component with styles and functionality
    
    * add CardData interface for card component type definition
    
    * add new images, styles, and components for landing and event sections
    
    * remove EventCard and MemberCard components along with their styles
    
    * Event page (#5)
    
    * feat: Add homepage layout with sections for landing, about, and carousel of events
    
    - Created `page.tsx` and `page.module.css` for the homepage layout.
    - Implemented `LandingSection`, `AboutSection`, and `CarouselSection` components.
    - Added `Card` component to display event details in the carousel.
    - Introduced responsive styles for various components.
    - Added button component with customizable styles and functionality.
    - Integrated social media icons in the footer.
    - Implemented language switcher in the navigation.
    - Set up YouTube video fetching functionality.
    - Established internationalization support with routing and middleware.
    
    * feat: integrate FullCalendar for event management
    
    - Added FullCalendar dependencies to package.json.
    - Created Calendar component to display events using FullCalendar.
    - Implemented API endpoint to fetch events from an ICS feed and parse them.
    - Added Modal component for displaying event details.
    - Updated Navigation component to improve accessibility and responsiveness.
    - Enhanced styles for carousel and calendar components.
    - Added landing image for better UI presentation.
    
    * feat: add newsletter section to homepage and update translations
    
    * Add About Page with styling and content
    
    - Created a new CSS module for the About page with styles for layout, typography, and responsive design.
    - Implemented the About page component in TypeScript, including an image and descriptive text.
    - Integrated internationalization support using next-intl for translations.
    
    * About page (#7)
    
    * refactor: restructure About page content and styles, update translations for landing, mission, and objectives sections
    
    * feat: add members section to About page with carousel of events
    
    * feat: add Coming Soon page with translations and styles, update navigation links

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,31 +1,133 @@
-// import { useTranslations } from "next-intl";
+import { useTranslations } from "next-intl";
 import styles from "./page.module.css";
 import Image from "next/image";
+import { FaGraduationCap, FaHandshake, FaNetworkWired } from "react-icons/fa";
+import Card from "../../_components/Card/Card";
+import CarouselSection from "../../_components/Page/CarouselSection";
 
 function AboutPage() {
-  // const t = useTranslations("aboutPage");
+  const t = useTranslations("aboutPage");
 
   return (
     <div className={styles.aboutPage}>
-      <section className={styles.landingSection}>
-        <div className={styles.imageContainer}>
-          <Image
-            width={300}
-            height={200}
-            src="/syria.png"
-            alt="Syrian Emergency Medicine Initiative"
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.textContent}>
-          <h1 className={styles.heading}>
-            Syrian Emergency Medicine Initiative
-          </h1>
-          <p className={styles.description}>
-          Uniting Syrian physicians worldwide to restore, strengthen, and lead the future of emergency medicine in Syria.
-          </p>
-        </div>
-      </section>
+      <div className="container">
+        <section className={styles.landingSection}>
+          <div className={styles.imageContainer}>
+            <Image
+              width={300}
+              height={200}
+              src="/syria.png"
+              alt="Syrian Emergency Medicine Initiative"
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.textContent}>
+            <h1 className={styles.heading}>{t("landingSection.title")}</h1>
+            <p className={styles.description}>
+              {t("landingSection.description")}
+            </p>
+          </div>
+        </section>
+        <section className={styles.missionSection}>
+          <h2 className="special-heading">{t("missionSection.title")}</h2>
+          <p>{t("missionSection.description")}</p>
+        </section>
+        <section className={styles.objectivesSection}>
+          <h2 className="special-heading">{t("objectivesSection.title")}</h2>
+          <ul>
+            <li>
+              <span>
+                <FaGraduationCap size={80} color="#4B7F52" />
+              </span>
+              <h3>
+                {t("objectivesSection.educateTitle")}
+              </h3>
+              <p>
+                {t("objectivesSection.educateDescription")}
+              </p>
+            </li>
+            <li>
+              <span>
+                <FaHandshake size={80} color="#4B7F52" />
+              </span>
+              <h3>
+                {t("objectivesSection.integrationTitle")}
+              </h3>
+              <p>
+                {t("objectivesSection.integrationDescription")}
+              </p>
+            </li>
+            <li>
+              <span>
+                <FaNetworkWired size={80} color="#4B7F52" />
+              </span>
+              <h3>
+                {t("objectivesSection.networkTitle")}
+              </h3>
+              <p>
+                {t("objectivesSection.networkDescription")}
+              </p>
+            </li>
+          </ul>
+        </section>
+        <CarouselSection
+        header={t("membersSection.title")}
+        items={[
+          <Card
+            cardData={{
+              imageUrl: "/landing.png",
+              title: "Emergency Medicine Workshop",
+              facebookUsername: "#",
+              instagramUsername: "#",
+              twitterUsername: "#",
+              linkedinUsername: "#",
+              description:
+                "Join us for a comprehensive workshop on the latest advancements in emergency medicine. This event is designed for healthcare professionals looking to enhance their skills and knowledge.",
+            }}
+            key={2}
+          />,
+          <Card
+            cardData={{
+              imageUrl: "/landing.png",
+              title: "Emergency Medicine Workshop",
+              facebookUsername: "#",
+              instagramUsername: "#",
+              twitterUsername: "#",
+              linkedinUsername: "#",
+              description:
+                "Join us for a comprehensive workshop on the latest advancements in emergency medicine. This event is designed for healthcare professionals looking to enhance their skills and knowledge.",
+            }}
+            key={3}
+          />,
+          <Card
+            cardData={{
+              imageUrl: "/landing.png",
+              title: "Emergency Medicine Workshop",
+              facebookUsername: "#",
+              instagramUsername: "#",
+              twitterUsername: "#",
+              linkedinUsername: "#",
+              description:
+                "Join us for a comprehensive workshop on the latest advancements in emergency medicine. This event is designed for healthcare professionals looking to enhance their skills and knowledge.",
+            }}
+            key={4}
+          />,
+          <Card
+            cardData={{
+              imageUrl: "/landing.png",
+              title: "ورشة عمل في طب الطوارئ",
+              facebookUsername: "#",
+              instagramUsername: "#",
+              twitterUsername: "#",
+              linkedinUsername: "#",
+              description:
+                "انضم إلينا في ورشة عمل شاملة حول أحدث التطورات في طب الطوارئ. تم تصميم هذا الحدث للمهنيين الصحيين الذين يتطلعون إلى تعزيز مهاراتهم ومعرفتهم.",
+            }}
+            key={5}
+          />,
+        ]}
+      />
+      </div>
     </div>
   );
 }
