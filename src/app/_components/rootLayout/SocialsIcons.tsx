a1ac68579ee23fe04b48bd0b3f3b4b5b29e0b7d1
Ayman Alkhatib <aemn22384@gmail.com>
2025-08-30 06:30:29 +0200

    fix: add Facebook icon and link to social icons

--- src/app/_components/rootLayout/SocialsIcons.tsx
+++ src/app/_components/rootLayout/SocialsIcons.tsx
@@ -1,11 +1,18 @@
 import styles from "./SocialsIcons.module.css";
-import { FaLinkedin } from "react-icons/fa";
+import { FaFacebookSquare, FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 function SocialsIcons() {
   return (
     <div className={styles.socials}>
       <span>Contact Us</span>
+      <a
+        href="https://www.facebook.com/profile.php?id=61579950531582"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaFacebookSquare size={22} color="black" />
+      </a>
       <a
         href="https://www.linkedin.com/company/syrian-emergency-medicine-initiative/"
         target="_blank"
